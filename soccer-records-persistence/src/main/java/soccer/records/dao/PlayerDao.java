3b46daff26ad7a37493aec7652e27247f94ea920
Radim Vidlák <vidlak@optimal-progam.cz>
2017-10-29 22:03:02 +0100

    New version of test for PlayerService and PlayerDao. Added method update to PlayerDao.

--- soccer-records-persistence/src/main/java/soccer/records/dao/PlayerDao.java
+++ soccer-records-persistence/src/main/java/soccer/records/dao/PlayerDao.java
@@ -7,6 +7,7 @@ import soccer.records.entity.Player;
 public interface PlayerDao {
     public Player findById(Long id);
     public void create(Player c);
+    public void update(Player c);
     public void delete(Player c);
     public List<Player> findAll();
     public List<Player> findByName(String name);
