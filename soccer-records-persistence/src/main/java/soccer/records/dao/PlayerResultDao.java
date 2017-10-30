c351ea1da46ffa86b45d6339201169c60faf937c
Tomáš Mazurek <tomas.mazurek1@gmail.com>
2017-10-30 23:56:16 +0100

    Javadoc added

--- soccer-records-persistence/src/main/java/soccer/records/dao/PlayerResultDao.java
+++ soccer-records-persistence/src/main/java/soccer/records/dao/PlayerResultDao.java
@@ -16,19 +16,44 @@ import soccer.records.entity.PlayerResult;
  * @author Radim Vidlák
  */
 public interface PlayerResultDao {
-
+    /**
+    * Method to create PlayerResult in db.
+    * @param pr PlayerResult to create
+    */
     public void create(PlayerResult pr);
-
+    /**
+    * Method to update PlayerResult in db.
+    * @param pr PlayerResult to update
+    */
     public void update(PlayerResult pr);
-
+    /**
+    * Method to delete PlayerResult from db.
+    * @param pr PlayerResult to delete
+    */
     public void delete(PlayerResult pr);
-
+    /**
+    * Method to find List of PlayerResults by Player.
+    * @param p Player of certain PlayerResult to find
+    * @return List of PlayerResults with certain player 
+    */
     public List<PlayerResult> findByPlayer(Player p);
-
+    /**
+    * Method to find List of PlayerResults by Match.
+    * @param m Player of certain PlayerResult to find
+    * @return List of PlayerResults with certain match 
+    */
     public List<PlayerResult> findByMatch(Match m);
-
+    /**
+    * Method to find PlayerResults by Player and Match.
+    * @param p Player of certain PlayerResult to find
+    * @param m Match of certain PlayerResult to find
+    * @return PlayerResult with certain player and match
+    */
     public PlayerResult findByBoth(Player p, Match m);
-    
+    /**
+    * Method to find all PlayerResults.
+    * @return return List of all teams
+    */
     public List<PlayerResult> findAll();
     
 }
--- soccer-records-persistence/src/main/java/soccer/records/dao/TeamDao.java
+++ soccer-records-persistence/src/main/java/soccer/records/dao/TeamDao.java
@@ -11,11 +11,37 @@ import soccer.records.entity.Team;
  */
 public interface TeamDao {
     
+        /**
+        * Method to find certain Team.
+        * @param id id of certain Team to find
+        * @return return Team with certain id
+         */
 	public Team findById(Long id);
+        /**
+        * Method to create Team in db.
+        * @param t Team to create
+         */
 	public void create(Team t);
+        /**
+        * Method to update Team in db.
+        * @param t Team to update
+         */
         public void update(Team t);
+        /**
+        * Method to delete Team from db.
+        * @param t Team to delete
+         */
 	public void delete(Team t);
+         /**
+        * Method to find all Teams.
+        * @return return List of all teams
+         */
 	public List<Team> findAll();
+        /**
+        * Method to update Team in db.
+        * @param name name of certain Team to find
+        * @return Team with certain name
+         */
         public Team findByName(String name);
     
 }
