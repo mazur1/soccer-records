36d42e07fd71ccc408dc118aa0f55a31ab9c241f
mimaboci <michaelabocanova@gmail.com>
2017-11-16 15:09:39 +0100

    Changed layout of the project

---
+++ soccer-records-service/src/main/java/soccer/records/config/ServiceConfiguration.java
@@ -0,0 +1,37 @@
+package soccer.records.config;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.dozer.loader.api.BeanMappingBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import soccer.records.PersistenceAppContext;
+
+@Configuration
+@Import(PersistenceAppContext.class)
+//@ComponentScan(basePackageClasses={})
+public class ServiceConfiguration {
+
+	@Bean
+	public Mapper dozer(){
+		DozerBeanMapper dozer = new DozerBeanMapper();		
+		//dozer.addMapping(new DozerCustomConfig());
+		return dozer;
+	}
+	
+	/**
+	 * Custom config for Dozer if needed
+	 * @author nguyen
+	 *
+	 */
+	/*public class DozerCustomConfig extends BeanMappingBuilder {
+	    @Override
+	    protected void configure() {
+	        mapping();
+	    }
+	}*/
+	
+}
+
