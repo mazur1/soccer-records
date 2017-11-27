8b6a69664c70089807f26080ee51df16b5b358b6
mimaboci <michaelabocanova@gmail.com>
2017-11-27 20:27:56 +0100

    facade tests

--- soccer-records-service/src/test/java/soccer/records/facade/PlayerFacadeTest.java
+++ soccer-records-service/src/test/java/soccer/records/facade/PlayerFacadeTest.java
@@ -5,10 +5,104 @@
  */
 package soccer.records.facade;
 
+import java.util.Arrays;
+import java.util.List;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import soccer.records.config.ServiceConfiguration;
+import soccer.records.dto.PlayerDto;
+import soccer.records.entity.Player;
+import soccer.records.services.BeanMappingService;
+import soccer.records.services.PlayerService;
+
 /**
- *
- * @author 
+ * Facade tests
+ * 
+ * @author Michaela Bocanova
  */
-public class PlayerFacadeTest {
+
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class PlayerFacadeTest extends AbstractTestNGSpringContextTests {
     
+    @Mock 
+    private PlayerService playerService;
+    
+    @Mock
+    private BeanMappingService mappingService;
+    
+    @Autowired
+    @InjectMocks
+    private PlayerFacade playerFacade;
+               
+    @BeforeClass
+    public void setup() //throws FacadeException 
+    {
+        MockitoAnnotations.initMocks(this);
+    }
+    @BeforeMethod
+    public void resetMock() {
+        Mockito.reset(playerService);
+    }
+    
+    @Mock
+    private PlayerDto pr1Dto;
+    @Mock
+    private Player pr1;
+ 
+    @Test
+    public void createPlayer() {
+        Mockito.when(mappingService.mapTo(pr1Dto, Player.class)).thenReturn(pr1);
+        playerFacade.createPlayer(pr1Dto);
+        Mockito.verify(playerService).create(pr1);
+    }
+    
+    //@Test
+    public void updatePlayer() {
+        Mockito.when(playerService.findById(pr1.getId())).thenReturn(pr1);
+        //playerFacade.updatePlayer(pr1Dto);
+        Mockito.verify(playerService).update(pr1);
+    }
+    
+    @Test
+    public void deletePlayer() {
+        Mockito.when(mappingService.mapTo(pr1Dto, Player.class)).thenReturn(pr1);
+        playerFacade.deletePlayer(pr1.getId());
+        Mockito.verify(playerService).remove(pr1);
+    }
+    
+    @Test
+    public void findAllPlayers() {
+    
+        List<Player> list = Arrays.asList(pr1);
+        Mockito.when(playerService.findAll()).thenReturn(list);
+        List<PlayerDto> listDto = Arrays.asList(pr1Dto);
+        Mockito.when(mappingService.mapTo(list, PlayerDto.class)).thenReturn(listDto);
+        
+        List<PlayerDto> actual = playerFacade.findAllPlayer();
+        Mockito.verify(playerService).findAll();
+        Mockito.verify(mappingService).mapTo(list, PlayerDto.class);
+        
+        Assert.assertEquals(actual, listDto);
+    }
+    
+    @Test
+    public void findPlayerById() {
+        Mockito.when(playerService.findById(pr1.getId())).thenReturn(pr1);
+        Mockito.when(mappingService.mapTo(pr1, PlayerDto.class)).thenReturn(pr1Dto);
+        
+        PlayerDto actual = playerFacade.findPlayerById(pr1.getId());
+        Mockito.verify(playerService).findById(pr1.getId());
+        Mockito.verify(mappingService).mapTo(pr1, PlayerDto.class);
+        
+        Assert.assertEquals(actual, pr1Dto);
+    }
 }
--- soccer-records-service/src/test/java/soccer/records/facade/TeamFacadeTest.java
+++ soccer-records-service/src/test/java/soccer/records/facade/TeamFacadeTest.java
@@ -5,10 +5,104 @@
  */
 package soccer.records.facade;
 
+import java.util.Arrays;
+import java.util.List;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import soccer.records.config.ServiceConfiguration;
+import soccer.records.dto.TeamDto;
+import soccer.records.entity.Team;
+import soccer.records.services.BeanMappingService;
+import soccer.records.services.TeamService;
+
 /**
- *
- * @author 
+ * Facade tests
+ * 
+ * @author Michaela Bocanova
  */
-public class TeamFacadeTest {
+
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class TeamFacadeTest extends AbstractTestNGSpringContextTests {
     
+    @Mock 
+    private TeamService teamService;
+    
+    @Mock
+    private BeanMappingService mappingService;
+    
+    @Autowired
+    @InjectMocks
+    private TeamFacade teamFacade;
+               
+    @BeforeClass
+    public void setup() //throws FacadeException 
+    {
+        MockitoAnnotations.initMocks(this);
+    }
+    @BeforeMethod
+    public void resetMock() {
+        Mockito.reset(teamService);
+    }
+    
+    @Mock
+    private TeamDto pr1Dto;
+    @Mock
+    private Team pr1;
+ 
+    @Test
+    public void createTeam() {
+        Mockito.when(mappingService.mapTo(pr1Dto, Team.class)).thenReturn(pr1);
+        teamFacade.createTeam(pr1Dto);
+        Mockito.verify(teamService).create(pr1);
+    }
+    
+    //@Test
+    public void updateTeam() {
+        Mockito.when(teamService.findById(pr1.getId())).thenReturn(pr1);
+        //teamFacade.updateTeam(pr1Dto);
+        Mockito.verify(teamService).update(pr1);
+    }
+    
+    @Test
+    public void deleteTeam() {
+        Mockito.when(mappingService.mapTo(pr1Dto, Team.class)).thenReturn(pr1);
+        teamFacade.deleteTeam(pr1.getId());
+        Mockito.verify(teamService).remove(pr1);
+    }
+    
+    @Test
+    public void findAllTeams() {
+    
+        List<Team> list = Arrays.asList(pr1);
+        Mockito.when(teamService.findAll()).thenReturn(list);
+        List<TeamDto> listDto = Arrays.asList(pr1Dto);
+        Mockito.when(mappingService.mapTo(list, TeamDto.class)).thenReturn(listDto);
+        
+        List<TeamDto> actual = teamFacade.findAllTeams();
+        Mockito.verify(teamService).findAll();
+        Mockito.verify(mappingService).mapTo(list, TeamDto.class);
+        
+        Assert.assertEquals(actual, listDto);
+    }
+    
+    @Test
+    public void findTeamById() {
+        Mockito.when(teamService.findById(pr1.getId())).thenReturn(pr1);
+        Mockito.when(mappingService.mapTo(pr1, TeamDto.class)).thenReturn(pr1Dto);
+        
+        TeamDto actual = teamFacade.findTeamById(pr1.getId());
+        Mockito.verify(teamService).findById(pr1.getId());
+        Mockito.verify(mappingService).mapTo(pr1, TeamDto.class);
+        
+        Assert.assertEquals(actual, pr1Dto);
+    }
 }
