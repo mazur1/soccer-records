c56b638c370e9e1b876badd12c9d1fe6a11c0a1f
Tomáš Mazurek <tomas.mazurek1@gmail.com>
2017-12-14 18:38:59 +0100

    team edit and create DTO

---
+++ soccer-records-api/src/main/java/soccer/records/dto/TeamEditDto.java
@@ -0,0 +1,90 @@
+
+package soccer.records.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Tomas Mazurek
+ */
+public class TeamEditDto {
+    
+    private String name;
+    
+    private List<PlayerDto> players = new ArrayList<PlayerDto>();
+    private List<MatchDto> matchesHome = new ArrayList<MatchDto>();
+    private List<MatchDto> matchesAway = new ArrayList<MatchDto>();
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public List<PlayerDto> getTeamPlayers() {
+        return this.players;
+    }
+    
+    public List<MatchDto> getMatchesHome() {
+        return this.matchesHome;
+    }
+    
+    public List<MatchDto> getMatchesAway() {
+        return this.matchesAway;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+             
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((players == null) ? 0 : players.hashCode());
+        result = prime * result + ((matchesHome == null) ? 0 : matchesHome.hashCode());
+        result = prime * result + ((matchesAway == null) ? 0 : matchesAway.hashCode());
+        return result;
+        
+    }   
+    
+    @Override
+    public boolean equals(Object obj) {
+        
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        TeamEditDto other = (TeamEditDto) obj;        
+        
+        if (name == null) {
+            if (other.getName() != null) {
+                return false;
+            }
+        } else if (!name.equals(other.getName())) {
+            return false;
+        }
+        
+        if (!matchesHome.equals(other.getMatchesHome())) {
+            return false;
+        }
+        
+        if (!matchesHome.equals(other.getMatchesAway())) {
+            return false;
+        }
+
+        if (!matchesHome.equals(other.getTeamPlayers())) {
+            return false;
+        }                       
+        
+        return true;
+    }    
+}
