bfe7a921abf0e645b9ffe176dac11f9ca90ab694
mimaboci <michaelabocanova@gmail.com>
2018-01-14 00:22:49 +0100

    activate in matches front fix

--- soccer-records-api/src/main/java/soccer/records/dto/MatchEditDto.java
+++ soccer-records-api/src/main/java/soccer/records/dto/MatchEditDto.java
@@ -30,6 +30,16 @@ public class MatchEditDto {
     private Long teamHomeId;
     
     private Long teamAwayId;
+    
+    protected boolean isActive;
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
 
     /*@JsonIgnore
     private List<Long> playerResults = new ArrayList<>();*/
